0b87ed9aefdb5fda6d70ac6a64cff2ec8c61b8ce
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-10-08 19:29:53 -0700

    wip

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import cx from 'classnames'
 
 const capitalizeString = (str: string): string => {
   const firstLetter = str.charAt(0)
@@ -13,10 +14,18 @@ const Home = () => {
   const [state, setState] = React.useState({
     direction: 'top',
     type: 'isosceles',
+    sizes: {
+      width: 0,
+      left: 0,
+      right: 0,
+      height: 0,
+      top: 0,
+      bottom: 0,
+    },
   })
   const isEquilateralAllowed = ORTHOGONAL_DIRECTIONS.includes(state.direction)
-  const position = state.direction + capitalizeString(state.type)
-  console.log('position:', position)
+  const positionAndType = state.direction + capitalizeString(state.type)
+  console.log('positionAndType:', positionAndType)
 
   const directionHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setState({
@@ -144,19 +153,18 @@ const Home = () => {
 
         {/* Type */}
         <div className="space-x-8 flex items-center">
-          {isEquilateralAllowed && (
-            <div className="flex items-center space-x-1">
-              <label htmlFor="type-equilateral">Equilateral</label>
-              <input
-                type="radio"
-                name="type"
-                id="type-equilateral"
-                value="equilateral"
-                onChange={typeHandler}
-                checked={state.type === 'equilateral'}
-              />
-            </div>
-          )}
+          <div className="flex items-center space-x-1">
+            <label htmlFor="type-equilateral">Equilateral</label>
+            <input
+              type="radio"
+              name="type"
+              id="type-equilateral"
+              value="equilateral"
+              onChange={typeHandler}
+              checked={state.type === 'equilateral'}
+              disabled={!isEquilateralAllowed}
+            />
+          </div>
           <div className="flex items-center space-x-1">
             <label htmlFor="type-isosceles">Isosceles</label>
             <input
@@ -180,6 +188,42 @@ const Home = () => {
             />
           </div>
         </div>
+
+        {/* Sizes */}
+        <div className="space-y-3">
+          {sizeInputFields.map((item) => {
+            return (
+              <label
+                key={item.key}
+                className={cx(
+                  'flex justify-end items-center bg-white z-10 relative space-x-3',
+                  !item.schema.includes(positionAndType) && 'opacity-25',
+                )}
+              >
+                <span className="capitalize">{item.key}:</span>
+                <input
+                  type="number"
+                  inputMode="numeric"
+                  pattern="[0-9]*"
+                  value={state.sizes[item.key]}
+                  max="200"
+                  min="1"
+                  onChange={(e) => {
+                    setState({
+                      ...state,
+                      sizes: {
+                        ...state.sizes,
+                        [item.key]: Number(e.target.value),
+                      },
+                    })
+                  }}
+                  disabled={!item.schema.includes(positionAndType)}
+                  className="w-16 md:w-20 flex-shrink-0 appearance-none rounded p-2 transition-colors duration-150 border border-gray-400 focus:outline-none"
+                />
+              </label>
+            )
+          })}
+        </div>
       </div>
     </>
   )
@@ -189,3 +233,76 @@ export default Home
 
 const ORTHOGONAL_DIRECTIONS = ['top', 'right', 'bottom', 'left']
 const DIAGONAL_DIRECTIONS = ['topRight', 'bottomRight', 'bottomLeft', 'topLeft']
+
+const SCHEMA_WIDTH = [
+  'topEquilateral',
+  'topIsosceles',
+  'topRightIsosceles',
+  'topRightScalene',
+  'rightIsosceles',
+  'rightScalene',
+  'bottomRightIsosceles',
+  'bottomRightScalene',
+  'bottomEquilateral',
+  'bottomIsosceles',
+  'bottomLeftIsosceles',
+  'bottomLeftScalene',
+  'leftIsosceles',
+  'leftScalene',
+  'topLeftIsosceles',
+  'topLeftScalene',
+]
+
+const SCHEMA_LEFT = ['topScalene', 'bottomScalene']
+
+const SCHEMA_RIGHT = ['topScalene', 'bottomScalene']
+
+const SCHEMA_HEIGHT = [
+  'topIsosceles',
+  'topScalene',
+  'topRightIsosceles',
+  'topRightScalene',
+  'rightEquilateral',
+  'rightIsosceles',
+  'bottomRightIsosceles',
+  'bottomRightScalene',
+  'bottomIsosceles',
+  'bottomScalene',
+  'bottomLeftIsosceles',
+  'bottomLeftScalene',
+  'leftEquilateral',
+  'leftIsosceles',
+  'topLeftIsosceles',
+  'topLeftScalene',
+]
+
+const SCHEMA_TOP = ['rightScalene', 'leftScalene']
+
+const SCHEMA_BOTTOM = ['rightScalene', 'leftScalene']
+
+const sizeInputFields = [
+  {
+    key: 'width',
+    schema: SCHEMA_WIDTH,
+  },
+  {
+    key: 'left',
+    schema: SCHEMA_LEFT,
+  },
+  {
+    key: 'right',
+    schema: SCHEMA_RIGHT,
+  },
+  {
+    key: 'height',
+    schema: SCHEMA_HEIGHT,
+  },
+  {
+    key: 'top',
+    schema: SCHEMA_TOP,
+  },
+  {
+    key: 'bottom',
+    schema: SCHEMA_BOTTOM,
+  },
+]
